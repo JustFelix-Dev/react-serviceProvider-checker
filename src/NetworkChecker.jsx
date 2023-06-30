c74dd05f220c491a6fcd0633d0a70348f488ddb0
Felix Owolabi <owolabifelix78@gmail.com>
2023-06-30 10:09:12 +0100

    service provider checker

--- src/NetworkChecker.jsx
+++ src/NetworkChecker.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import 'yup-phone-lite'
+import { Form, useFetcher } from 'react-router-dom'
 
 const initialValues = {
     myInput: '',
@@ -20,6 +21,8 @@ const NetworkChecker = () => {
     const [ myInput,setMyInput ] = useState('');
     const [ nspName,setNspName] = useState('');
     const imageRef = useRef()
+    const fetcher = useFetcher()
+    
 
     const formik = useFormik({
         initialValues,
@@ -30,22 +33,22 @@ const NetworkChecker = () => {
             let finishedValue = updatedValue.substring(1,12);
               setMyInput(finishedValue)
             if(finishedValue.startsWith('0814')||finishedValue.startsWith('0803')||finishedValue.startsWith('0703')||finishedValue.startsWith('0706')||finishedValue.startsWith('0813')||finishedValue.startsWith('0816')||finishedValue.startsWith('0810')||finishedValue.startsWith('0814')||finishedValue.startsWith('0903')||finishedValue.startsWith('0906')||finishedValue.startsWith('0913')||finishedValue.startsWith('0916')||finishedValue.startsWith('07025')||finishedValue.startsWith('07026')||finishedValue.startsWith('0704')){
-                     setNspName('MTN')
+                     setNspName('MTN :')
                     imageRef.current.style.display = 'block';
                     imageRef.current.src = 'images/mtnLogo.png';
             }
             else if(finishedValue.startsWith('0805')||finishedValue.startsWith('0807')||finishedValue.startsWith('0705')||finishedValue.startsWith('0815')||finishedValue.startsWith('0811')||finishedValue.startsWith('0905')||finishedValue.startsWith('0915')){
-                      setNspName('GLO')
+                      setNspName('GLO :')
                      imageRef.current.style.display = 'block';
                     imageRef.current.src = 'images/gloLogo.jpg';
             }
             else if(finishedValue.startsWith('0802')||finishedValue.startsWith('0808')||finishedValue.startsWith('0708')||finishedValue.startsWith('0812')||finishedValue.startsWith('0701')||finishedValue.startsWith('0902')||finishedValue.startsWith('0901')||finishedValue.startsWith('0904')||finishedValue.startsWith('0907')||finishedValue.startsWith('0912')){
-                     setNspName('AIRTEL')
+                     setNspName('AIRTEL :')
                     imageRef.current.style.display = 'block';
                     imageRef.current.src = 'images/airtelLogo.jpg';
             }
             else if(finishedValue.startsWith('0809')||finishedValue.startsWith('0818')||finishedValue.startsWith('0817')||finishedValue.startsWith('0909')||finishedValue.startsWith('0908')){
-                        setNspName('9mobile')
+                        setNspName('9mobile :')
                     imageRef.current.style.display = 'block';
                     imageRef.current.src = 'images/9mobileLogo.png';
             }
@@ -59,10 +62,9 @@ const NetworkChecker = () => {
   return (
          <>
              <div className="container">
-
            <div className="form__wrapper">
-            <form action="" onSubmit={formik.handleSubmit}>
-            <h2>Network Service Provider Checker</h2>
+            <fetcher.Form action="" onSubmit={formik.handleSubmit}>
+           <marquee behavior="alternate" direction="left"><h2>Network Service Provider Checker</h2></marquee> 
               <input type="text"
                inputMode='numeric' 
                placeholder='Check your Network Provider'
@@ -71,13 +73,15 @@ const NetworkChecker = () => {
                 />
                 {<code>{formik.touched.myInput && formik.errors.myInput}</code>}
                 <button type='submit'>Validate</button>
-            </form>
+            </fetcher.Form>
            </div>
 
             <div className="display__image">
-                <img src={''} ref= {imageRef} className='display__Image' alt='serviceProvider_Image' height={150} width={150} />
+                <img src={''} ref= {imageRef} style={{borderRadius:"9px"}} className='display__Image' alt='serviceProvider_Image' height={150} width={150} />
             </div>   
-            {nspName} : {myInput}           
+             <div className="output_text">
+                         {nspName}  {myInput} 
+                </div>           
              </div>
          </>
   )
