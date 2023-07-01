f9194a81132bd7bff602bfe4b88e8868dce43acf
Felix Owolabi <owolabifelix78@gmail.com>
2023-07-01 12:57:19 +0100

    nsp checker

--- src/NetworkChecker.jsx
+++ src/NetworkChecker.jsx
@@ -11,9 +11,9 @@ const initialValues = {
 
 const validationSchema = Yup.object().shape({
     myInput:Yup.string().phone('NG','Please enter a valid phone Number')
-                        .min(10,'Complete your 11 digits')
-                        .max(11,'More than 11 digits!')
-               .required('Enter Your 11 digits')
+                        .min(9,'Complete your 10 digits')
+                        .max(10,'More than 10 digits!')
+               .required('Enter Your 10 digits')
 
 })
 
@@ -28,47 +28,49 @@ const NetworkChecker = () => {
     const imageRef = useRef()
           
     const handleClick=async(value)=>{
+                  setNspName('')
+                  setMyInput('')
                 setIsLoading(true)
                 await waait()
                 let parsedValue = JSON.stringify(value);
                 let processedValue = parsedValue.split(':')[1];
                 let updatedValue = processedValue.split('}')[0];
-                let finishedValue = updatedValue.substring(1,12);
+                let finishedValue = updatedValue.substring(1,11);
                   setMyInput(finishedValue)
-                if(finishedValue.startsWith('0814')||finishedValue.startsWith('0803')||finishedValue.startsWith('0703')||finishedValue.startsWith('0706')||finishedValue.startsWith('0813')||finishedValue.startsWith('0816')||finishedValue.startsWith('0810')||finishedValue.startsWith('0814')||finishedValue.startsWith('0903')||finishedValue.startsWith('0906')||finishedValue.startsWith('0913')||finishedValue.startsWith('0916')||finishedValue.startsWith('07025')||finishedValue.startsWith('07026')||finishedValue.startsWith('0704')){
+                if(finishedValue.startsWith('814')||finishedValue.startsWith('803')||finishedValue.startsWith('703')||finishedValue.startsWith('706')||finishedValue.startsWith('813')||finishedValue.startsWith('816')||finishedValue.startsWith('810')||finishedValue.startsWith('814')||finishedValue.startsWith('903')||finishedValue.startsWith('0906')||finishedValue.startsWith('913')||finishedValue.startsWith('916')||finishedValue.startsWith('7025')||finishedValue.startsWith('7026')||finishedValue.startsWith('704')){
                          setIsLoading(false)
                          setIsHighlighted(true)
-                         setNspName('MTN :')
+                         setNspName('MTN : 0')
                          setMySrc('images/mtnLogo.png')
                          setNotFound('')
                 }
-                else if(finishedValue.startsWith('0805')||finishedValue.startsWith('0807')||finishedValue.startsWith('0705')||finishedValue.startsWith('0815')||finishedValue.startsWith('0811')||finishedValue.startsWith('0905')||finishedValue.startsWith('0915')){
+                else if(finishedValue.startsWith('805')||finishedValue.startsWith('807')||finishedValue.startsWith('705')||finishedValue.startsWith('815')||finishedValue.startsWith('811')||finishedValue.startsWith('905')||finishedValue.startsWith('915')){
                         setIsLoading(false)
                         setIsHighlighted(true)
-                        setNspName('GLO :')
+                        setNspName('GLO : 0')
                         setMySrc('images/gloLogo.jpg')
                         setNotFound('')
 
                 }
-                else if(finishedValue.startsWith('0802')||finishedValue.startsWith('0808')||finishedValue.startsWith('0708')||finishedValue.startsWith('0812')||finishedValue.startsWith('0701')||finishedValue.startsWith('0902')||finishedValue.startsWith('0901')||finishedValue.startsWith('0904')||finishedValue.startsWith('0907')||finishedValue.startsWith('0912')){
+                else if(finishedValue.startsWith('802')||finishedValue.startsWith('808')||finishedValue.startsWith('708')||finishedValue.startsWith('812')||finishedValue.startsWith('701')||finishedValue.startsWith('902')||finishedValue.startsWith('901')||finishedValue.startsWith('904')||finishedValue.startsWith('907')||finishedValue.startsWith('912')){
                         setIsLoading(false)
                         setIsHighlighted(true)
-                        setNspName('AIRTEL :')
+                        setNspName('AIRTEL : 0')
                         setMySrc('images/airtelLogo.jpg')
                         setNotFound('')
 
                 }
-                else if(finishedValue.startsWith('0809')||finishedValue.startsWith('0818')||finishedValue.startsWith('0817')||finishedValue.startsWith('0909')||finishedValue.startsWith('0908')){
+                else if(finishedValue.startsWith('809')||finishedValue.startsWith('818')||finishedValue.startsWith('817')||finishedValue.startsWith('909')||finishedValue.startsWith('908')){
                             setIsLoading(false)
                             setIsHighlighted(true)
-                            setNspName('9mobile :')
+                            setNspName('9mobile : 0')
                             setMySrc('images/9mobileLogo.png')
                             setNotFound('')
 
                 }
                 else{
                         setIsLoading(false)
-                        setNspName('Not Found :')
+                        setNspName('Not Found : 0')
                         setNotFound("Sorry, Number not found!")
                         setMySrc('images/unknown.png')
 
@@ -87,40 +89,65 @@ const NetworkChecker = () => {
              <div className="container">
            <div className="form__wrapper">
             <form action="" onSubmit={formik.handleSubmit}>
-           <marquee behavior="alternate" direction="left"><h2> Service Provider Checker</h2></marquee> 
-              <input type="text"
-               inputMode='numeric' 
-               placeholder='Check your Network Provider'
-               name='myInput'
-                {...formik.getFieldProps('myInput')}
-                />
-                {<code>{formik.touched.myInput && formik.errors.myInput}</code>}
+           <marquee behavior="scroll" direction="right"><h2> Service Provider Checker</h2></marquee> 
+           <div className="ui-wrapper">
+                <input checked="" id="Nigeria" name="flag" type="radio"/>
+                <input className="dropdown-checkbox" name="dropdown" id="dropdown" type="checkbox"/>
+                <label className="dropdown-container" htmlFor="dropdown"></label>
+                <div className="input-wrapper">
+                    <legend>
+                        <label for="phonenumber">
+                        Phone Number*
+                        </label>
+                    </legend>
+                    <div className="textfield">
+                        <input pattern="\d+" maxLength="10" inputMode='numeric'
+                          autoComplete='off'
+                         id="phonenumber" type="text"
+                         name='myInput'
+                         {...formik.getFieldProps('myInput')}/>
+                    </div>
+                </div>
+                <div className="select-wrapper">
+                    <ul>
+                        <li className="Nigeria"><label for="Nigeria"><span>NG</span>Nigeria (+234)</label></li>
+                    </ul>
+                </div>
+                </div>
                 <button type='submit'> {
-                        isLoading ? <span>Submitting...</span> : <span>Validate</span>
-                }</button>
+                    isLoading ? (<div class="spinner">
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                </div>) : <span>Validate</span>
+                }</button>  <br />
+                {<span className='invalid'>{formik.touched.myInput && formik.errors.myInput}</span>}
             </form>
            </div>
 
             <div className="display__image">
                 {
-                    isLoading ? ( <div className="hourglassBackground">
-                    <div className="hourglassContainer">
-                      <div className="hourglassCurves"></div>
-                      <div className="hourglassCapTop"></div>
-                      <div className="hourglassGlassTop"></div>
-                      <div className="hourglassSand"></div>
-                      <div className="hourglassSandStream"></div>
-                      <div className="hourglassCapBottom"></div>
-                      <div className="hourglassGlass"></div>
-                    </div>
-                  </div>):(
+                    isLoading ? (<div class="three-body">
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                    </div>):(
                      <img src={mySrc} ref= {imageRef} style={{borderRadius:"9px",display: isHighlighted ? 'block':'none'}} className='display__Image' alt='serviceProvider_Image' height={150} width={150} />
                   )
                 }
                
             </div>   
              <div className="output_text">
-                         {nspName}  {myInput} <br/>
+                         {nspName}{myInput} <br/>
                          {notFound}
                 </div>           
              </div>
